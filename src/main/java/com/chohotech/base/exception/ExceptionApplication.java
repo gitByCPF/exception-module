2d901cc962c40981f8b15a9371fa0e4afcb29ab9
CuiPengfei <cpfprogrammer@163.com>
2022-11-04 17:32:00 +0800

    main方法是必须的

---
+++ src/main/java/com/chohotech/base/exception/ExceptionApplication.java
@@ -0,0 +1,15 @@
+package com.chohotech.base.exception;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author cpf email: cpfprogrammer@163.com
+ * {@code @date} 2022/11/4 下午5:31
+ */
+@SpringBootApplication
+public class ExceptionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExceptionApplication.class, args);
+    }
+}
